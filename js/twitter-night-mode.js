d871b576d63549a3e7cfa56d52ee91603c6f671b
AOEUPL PHAOE <AOEUPL.PHAOE@gmail.com>
2017-01-04 21:14:59 -0600

    More efficiency

--- js/twitter-night-mode.js
+++ js/twitter-night-mode.js
@@ -7,8 +7,9 @@ chrome.storage.local.get({'nightmode': true}, function(items) {
 function setNightMode() {
   var regex = /https:\/\/twitter\.com\/(.*)/;
 
-  if(regex.test(document.URL)) {
+  if (regex.test(document.URL) && document.getElementById('custom-style-twitter-dark-mode') === null) {
     var style = document.createElement('link');
+    style.id = 'custom-style-twitter-dark-mode';
     style.rel = 'stylesheet';
     style.type = 'text/css';
     style.href = chrome.extension.getURL('css/main.css');
